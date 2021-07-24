9a7054d2c8ac7c3aea9fbe23b83583fb150bdd80
eric-ding <eric-ding@users.noreply.github.com>
2021-07-24 20:16:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,3 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git njit https://github.com/liuqun/njit8021xclient >>feeds.conf.default
